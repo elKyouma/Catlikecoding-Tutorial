435ef419cf5dbbb9b904d28491ca5a47adfbfa1a
elKyouma <adrian2402glizniewicz@gmail.com>
2024-02-26 19:15:07 +0100

    Proper UVSphere scaling

--- Assets/MeshGeneration/UVSphere.cs
+++ Assets/MeshGeneration/UVSphere.cs
@@ -10,29 +10,31 @@ namespace ProceduralMeshes.Generators
     public struct UVSphere : IMeshGenetator
     {
         public int Resolution { get; set; }
-        public int JobLength => Resolution + 1;
-        public int VertexCount => (Resolution + 1) * (Resolution + 1);
-        public int IndexTriangleCount => 6 * Resolution * Resolution;
+        public int ResolutionU => Resolution * 4;
+        public int ResolutionV => Resolution * 2;
+        public int JobLength => ResolutionV + 1;
+        public int VertexCount => (ResolutionU + 1) * (ResolutionV + 1);
+        public int IndexTriangleCount => 6 * ResolutionU * ResolutionV;
 
         public Bounds Bounds => new(new Vector3(0.5f, 0f, 0.5f), new Vector3(1f, 1f, 1f));
 
-        private int GetIndex(float x, float z) => (int)x + (int)z * (Resolution + 1);
+        private int GetIndex(float x, float z) => (int)x + (int)z * (ResolutionU + 1);
         public void Execute<Streams>(int index, Streams streams) where Streams : struct, IMeshStreams
         {
             Vertex tempVertex;
 
             float z = index;
-            int ti = 2 * Resolution * (index - 1) - 2;
-            for (int i = 0; i < Resolution + 1; i++, ti += 2)
+            int ti = 2 * ResolutionU * (index - 1) - 2;
+            for (int i = 0; i < ResolutionU + 1; i++, ti += 2)
             {
                 float x = i;
 
-                tempVertex.position = float3(cos(2 * PI * x / Resolution) * sin(PI * z / Resolution),
-                                                                            -cos(PI * z / Resolution),
-                                            sin(2 * PI * x / Resolution) * sin(PI * z / Resolution));
+                tempVertex.position = float3(cos(2 * PI * x / ResolutionU) * sin(PI * z / ResolutionV),
+                                                                            -cos(PI * z / ResolutionV),
+                                            sin(2 * PI * x / ResolutionU) * sin(PI * z / ResolutionV));
                 tempVertex.normal = normalize(tempVertex.position);
                 tempVertex.tangent = float4(cross(float3(tempVertex.normal.x, 0f, tempVertex.normal.z), up()), -1f);
-                tempVertex.uv = float2(x / Resolution, z / Resolution);
+                tempVertex.uv = float2((x / ResolutionU), z / ResolutionV);
                 streams.SetVertex(GetIndex(x, z), tempVertex);
 
                 if (z > 0 && x > 0)
